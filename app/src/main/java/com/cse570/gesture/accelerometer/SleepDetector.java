27a1c077ca6157e4a900593e8a49a15579fb520b
binarybleed <hiprateekroy@gmail.com>
2017-12-20 05:32:00 -0500

    Update

--- app/src/main/java/com/cse570/gesture/accelerometer/SleepDetector.java
+++ app/src/main/java/com/cse570/gesture/accelerometer/SleepDetector.java
@@ -18,16 +18,11 @@ public class SleepDetector implements SensorEventListener {
     private PowerManager.WakeLock wakeLock;
     private int field = 0x00000020;
 
-    private float lastX, lastY, lastZ;
-    private float changeinXMax = 0;
-    private float changeinYMax = 0;
-    private float changeinZMax = 0;
     private float changeinX = 0;
     private float changeinY = 0;
     private float changeinZ = 0;
     private long lastUpdate = 0;
-    private float vibrateThreshold = 0;
-    private TextView accelerationX, accelerationY, accelerationZ, maxX, maxY, maxZ;
+    private TextView accelerationX, accelerationY, accelerationZ;
     public Vibrator v;
     Context context;
     Activity activity;
@@ -49,7 +44,7 @@ public class SleepDetector implements SensorEventListener {
         powerManager = _powerManager;
         mAccelerometer = _mAccelerometer;
         wakeLock = powerManager.newWakeLock(field, _classname);
-        vibrateThreshold = mAccelerometer.getMaximumRange() / 2;
+
 
         v = _vibrator;
         context = _context;
@@ -64,9 +59,6 @@ public class SleepDetector implements SensorEventListener {
         accelerationX = (TextView) activity.findViewById(R.id.accelerationX);
         accelerationY = (TextView) activity.findViewById(R.id.accelerationY);
         accelerationZ = (TextView) activity.findViewById(R.id.accelerationZ);
-        maxX = (TextView) activity.findViewById(R.id.maxX);
-        maxY = (TextView) activity.findViewById(R.id.maxY);
-        maxZ = (TextView) activity.findViewById(R.id.maxZ);
     }
 
     @Override
@@ -89,20 +81,11 @@ public class SleepDetector implements SensorEventListener {
             displayCleanValues();
             // display the acceleration x,y,z accelerometer values
             displayaccelerationValues();
-            // display the max x,y,z accelerometer values
-            displayMaxValues();
             // get the change of the x,y,z values of the accelerometer
-            changeinX = Math.abs(lastX - event.values[0]);
-            changeinY = Math.abs(lastY - event.values[1]);
-            changeinZ = Math.abs(lastZ - event.values[2]);
-            // if the change is below 2, it is just plain noise
-            if (changeinX < 2)
-                changeinX = 0;
-            if (changeinY < 2)
-                changeinY = 0;
-            if ((changeinZ > vibrateThreshold) || (changeinY > vibrateThreshold) || (changeinZ > vibrateThreshold)) {
-                v.vibrate(50);
-            }
+            changeinX = event.values[0];
+            changeinY = event.values[1];
+            changeinZ = event.values[2];
+
             if (event.values[2] <= 0) {
 
                 if (!wakeLock.isHeld()) {
@@ -131,19 +114,4 @@ public class SleepDetector implements SensorEventListener {
         accelerationZ.setText(Float.toString(changeinZ));
     }
 
-    // display the max x,y,z accelerometer values
-    public void displayMaxValues() {
-        if (changeinX > changeinXMax) {
-            changeinXMax = changeinX;
-            maxX.setText(Float.toString(changeinXMax));
-        }
-        if (changeinY > changeinYMax) {
-            changeinYMax = changeinY;
-            maxY.setText(Float.toString(changeinYMax));
-        }
-        if (changeinZ > changeinZMax) {
-            changeinZMax = changeinZ;
-            maxZ.setText(Float.toString(changeinZMax));
-        }
-    }
 }
